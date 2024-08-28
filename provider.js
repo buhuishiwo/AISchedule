2ffc5a0a1693c7618c744022cee4fd107bf09f71
Mesonsaber <1437317989@qq.com>
2024-08-28 17:48:06 +0800

    new

---
+++ provider.js
@@ -0,0 +1,16 @@
+function scheduleHtmlProvider(iframeContent = "", frameContent = "") {
+    var sch = document.querySelector('#kblist_table')
+    if (!sch) {
+          let TriPrompto = `
+          没有获取到课表哦
+          导入流程：
+           >> 输入账号密码 账号是学号
+           >> 点击右上角头像旁三条横线
+           >> 依次: 选课>学生课表查询
+           >> 点击<一键导入>
+          `
+          alert(TriPrompto)
+    }
+    return sch.outerHTML
+}
+
